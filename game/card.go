91fb82899e1bacda8fd957b6917f9d10e7b08bb9
Willem van der Schyff <willemvds@gmail.com>
2014-10-29 22:46:11 +0200

    Looks like we'll be
    (•_•)
    ( •_•) ⌐■-■
    (⌐■_■)
    
    stacking cards

--- game/card.go
+++ game/card.go
@@ -1,5 +1,12 @@
 package game
 
+import (
+	"errors"
+)
+
+var ErrNoMoreCards = errors.New("No cards left on stack")
+var ErrTooManyCards = errors.New("Too many cards on stack")
+
 type cardrank uint8
 
 func (cr cardrank) Next() cardrank {
@@ -26,9 +33,48 @@ func NewCard(rank uint8) *card {
 	return &c
 }
 
+func (c *card) String() string {
+	if c == nil {
+		return "I appear to be a <nil> '*card'"
+	}
+	return "Hello, I am a Card."
+}
+
 func (c *card) NextTo(targetCard *card) bool {
 	if c.rank == targetCard.rank.Next() || c.rank == targetCard.rank.Prev() {
 		return true
 	}
 	return false
+}
+
+type cardstack struct {
+	stack []*card
+	size  int
+}
+
+func NewCardStack(size int) *cardstack {
+	cs := cardstack{}
+	cs.stack = make([]*card, 0, size)
+	cs.size = size
+	return &cs
+}
+
+func (cs *cardstack) Push(c *card) error {
+	if len(cs.stack) < cs.size {
+		cs.stack = append(cs.stack, c)
+		return nil
+	}
+	return ErrNoMoreCards
+}
+
+func (cs *cardstack) droplast() {
+	cs.stack = cs.stack[:len(cs.stack)-1]
+}
+
+func (cs *cardstack) Pop() (*card, error) {
+	if len(cs.stack) > 0 {
+		defer cs.droplast()
+		return cs.stack[len(cs.stack)-1], nil
+	}
+	return nil, ErrTooManyCards
 }
